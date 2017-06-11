21ac7b3e80e92ffb46fedfc0504f133da57be0dd
sjdodge123 <sjdodge@outlook.com>
2017-06-11 17:41:56 -0400

    fixed an issue with the prod deployment script

--- deployToProd.sh
+++ deployToProd.sh
@@ -1,1 +1,1 @@
-scp -r /home/ubuntu/spaceroyale.io/ ec2-54-197-221-219.compute-1.amazonaws.com:/home/ubuntu/spaceroyale.io/
+scp -r /home/ubuntu/spaceroyale.io/ ec2-54-197-221-219.compute-1.amazonaws.com:/home/ubuntu/
