85fa834e541442612aee840b088167a2c48238a4
rtoole13 <rtoole13@gmail.com>
2017-06-11 20:09:08 -0400

    More to the sandbox..
    Added a collision test tool. Currently, attaching a circle to mouseX, mouseY.
    Checking: circle v circle and circle v rect (any angle rect)
    To add: rect vs rect (any angle vs any angle)
    
    Currently, the circle on rect stuff is needlessly complex. It checks to see if circle x,y is in rect, if no:
    checks each rect line segment to see if it intersects circle. if no:
    check circle x,y vs each rect vertex to see if corner case.
    
    I'd like to add (over a period of time), many different methods of checking circle vs rect, rect vs rect, etc. to see how they perform vs one another.
    One thing that will surely work better:
    In circle vs rect, rotate circle x,y about rectangle's center. Then check collision against axes-aligned rect.

---
+++ sandbox/engine.js
@@ -0,0 +1,93 @@
+"use strict";
+
+function circleOnCircle(circleA, circleB){
+    var distsq = getDistanceSq(circleA.x, circleA.y, circleB.x, circleB.y);
+    if (distsq <= Math.pow(circleA.radius + circleB.radius, 2)){
+        return true;
+    }
+    return false;
+}
+
+function circleOnRect(circleA, rectB){
+    var vertices = [];
+    //set up relative vertices
+    var a = {x:-rectB.width/2, y: -rectB.height/2},
+        b = {x:rectB.width/2, y: -rectB.height/2},
+        c = {x:rectB.width/2, y: rectB.height/2},
+        d = {x:-rectB.width/2, y: rectB.height/2};
+    vertices.push(a, b, c, d);
+    var cos = Math.cos(rectB.angle * Math.PI/180);
+    var sin = Math.sin(rectB.angle * Math.PI/180);
+
+    var tempX, tempY;
+    for (var i = 0; i < vertices.length; i++){
+        var vert = vertices[i];
+        tempX = vert.x * cos - vert.y * sin;
+        tempY = vert.x * sin + vert.y * cos;
+        vert.x = rectB.x + tempX;
+        vert.y = rectB.y + tempY;
+    }
+    if(pointInRect(circleA.x, circleA.y, vertices)){
+        return true;
+    }
+    if(lineIntersectCircle(circleA, vertices[0], vertices[1]) ||
+       lineIntersectCircle(circleA, vertices[1], vertices[2]) ||
+       lineIntersectCircle(circleA, vertices[2], vertices[3]) ||
+       lineIntersectCircle(circleA, vertices[3], vertices[0])){
+        return true;
+    }
+
+    for (var i = 0; i < vertices.length; i++){
+        var distsq = getDistanceSq(circleA.x, circleA.y, vertices[i].x, vertices[i].y);
+        if (distsq < Math.pow(circleA.radius, 2)){
+            return true;
+        }
+    }
+    return false;
+}
+
+function rectOnRect(rectA, rectB){
+    return false;
+}
+
+function pointInRect(x, y, vertices){
+    var ap = {x:x-vertices[0].x,y:y-vertices[0].y};
+    var ab = {x:vertices[1].x - vertices[0].x, y:vertices[1].y - vertices[0].y};
+    var ad = {x:vertices[3].x - vertices[0].x, y:vertices[3].y - vertices[0].y};
+
+	var dotW = dotProduct(ap, ab);
+	var dotH = dotProduct(ap, ad);
+	if ((0 <= dotW) && (dotW <= dotProduct(ab, ab)) && (0 <= dotH) && (dotH <= dotProduct(ad, ad))){
+		return true;
+	}
+    return false;
+}
+
+function lineIntersectCircle(circle, a, b){
+
+    var ap, ab, dirAB, magAB, projMag, perp, perpMag;
+    ap = {x: circle.x - a.x, y: circle.y - a.y};
+    ab = {x: b.x - a.x, y: b.y - a.y};
+    magAB = Math.sqrt(dotProduct(ab,ab));
+    dirAB = {x: ab.x/magAB, y: ab.y/magAB};
+
+    projMag = dotProduct(ap, dirAB);
+
+    perp = {x: ap.x - projMag*dirAB.x, y: ap.y - projMag*dirAB.y};
+    perpMag = Math.sqrt(dotProduct(perp, perp));
+    if ((0 < perpMag) && (perpMag < circle.radius) && (0 <  projMag) && (projMag < magAB)){
+        return true;
+    }
+    return false;
+}
+function getDistance(x1, y1, x2, y2){
+    return Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1, 2));
+}
+
+function getDistanceSq(x1, y1, x2, y2){
+    return Math.pow(x2-x1,2) + Math.pow(y2-y1, 2);
+}
+
+function dotProduct(a, b){
+    return a.x * b.x + a.y * b.y;
+}
