07cecba08bebee2ff5ba87339d94a3c4cd1b586b
sjdodge123 <sjdodge@outlook.com>
2017-07-03 17:03:59 -0400

    fixed deployment path

--- deployToProd.sh
+++ deployToProd.sh
@@ -1,1 +1,1 @@
-scp -r /home/ubuntu/spaceroyale.io/ ec2-54-197-221-219.compute-1.amazonaws.com:/home/ubuntu/
+scp -r /home/ubuntu/spaceroyale.io/ ec2-52-90-214-46.compute-1.amazonaws.com:/home/ubuntu/
